e8b5c8043a627cf334c18561478e4a2aa9f1076e
Flores Gómez Adalid <102763145+FloresGomezAdalid@users.noreply.github.com>
2022-04-05 01:23:55 -0400

    Add files via upload

---
+++ PSIS457LAB0102/Principal.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char v = 'v';
+	char s = 's';
+
+	while (v == 'v') {
+
+		cout << "                  Biografia                      " << endl;
+		cout << "             Adalid Flores Gómez               \n" << endl;
+		cout << "Mi nombre es Adalid Flores Gómez, tengo 19 años," << endl;
+		cout << "nací el 10 de Enero de 2003 en la ciudad de Potosi" << endl;
+		cout << "Vivo con mis padres Mayra Gómez , Braulio Flores" << endl;
+		cout << "y mi hermana Valeria de 11 años. " << endl;
+		cout << "De religión católica, cumplí los 3 primeros" << endl;
+		cout << "sacramentos, actualmente vivo en la Ciudad de" << endl;
+		cout << "Sucre, el nivel inicial lo realicé en el kinder " << endl;
+		cout << "Jaime Mendoza de la ciudad de Potosi, el nivel " << endl;
+		cout << "primario lo realicé en la escuela Don Bosco de la" << endl;
+		cout << "ciudad de Potosi, el nivel secundario lo realicé en " << endl;
+		cout << "el colegio Nacional Junin de la ciudad de Sucre, tengo" << endl;
+		cout << "un título en técnico medio en electricidad." << endl;
+		cout << "Estudio la carrera de Ingenieria de Sistemas, mis" << endl;
+		cout << "pasatiempos son jugar videojuegos ,ayudar  a mi" << endl;
+		cout << "familia en casa, mi color favorito es el color negro." << endl;
+		cout << "No realizo ningun deporte debido a un  accidente" << endl;
+		cout << "que tuve hace unos años atrás, donde fue afectado mi "<< endl;
+		cout << "pie izquierdo, mi juego favorito es Minecraft, uno de" << endl;
+		cout << "mis sueños es poder programar mods y videjuegos\n" << endl;
+		cout << "Desea volver a ver la biografía oprima la (v/V) Para salir oprima la (s/S) para salir" << endl;
+		cin >> v;
+
+	}
+	return 0;
+}
