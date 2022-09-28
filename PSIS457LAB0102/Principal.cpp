cbb52dd22de82890e09f71d10c124487f5d6ea96
Flores Gómez Adalid <102763145+FloresGomezAdalid@users.noreply.github.com>
2022-09-28 09:12:11 -0400

    Update Principal.cpp

--- PSIS457LAB0102/Principal.cpp
+++ PSIS457LAB0102/Principal.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 int main()
 {
-	char v = 'v';
 	char s = 's';
+	char n = 'n';
 
-	while (v == 'v') {
+	while (s == 's') {
 
 		cout << "                  Biografia                      " << endl;
-		cout << "             Adalid Flores Gómez               \n" << endl;
-		cout << "Mi nombre es Adalid Flores Gómez, tengo 19 años," << endl;
-		cout << "nací el 10 de Enero de 2003 en la ciudad de Potosi" << endl;
-		cout << "Vivo con mis padres Mayra Gómez , Braulio Flores" << endl;
-		cout << "y mi hermana Valeria de 11 años. " << endl;
-		cout << "De religión católica, cumplí los 3 primeros" << endl;
+		cout << "             Adalid Flores GÃ³mez               \n" << endl;
+		cout << "Mi nombre es Adalid Flores GÃ³mez, tengo 19 aÃ±os," << endl;
+		cout << "nacÃ­ el 10 de Enero de 2003 en la ciudad de Potosi" << endl;
+		cout << "Vivo con mis padres Mayra GÃ³mez , Braulio Flores" << endl;
+		cout << "y mi hermana Valeria de 11 aÃ±os. " << endl;
+		cout << "De religiÃ³n catÃ³lica, cumplÃ­ los 3 primeros" << endl;
 		cout << "sacramentos, actualmente vivo en la Ciudad de" << endl;
-		cout << "Sucre, el nivel inicial lo realicé en el kinder " << endl;
+		cout << "Sucre, el nivel inicial lo realicÃ© en el kinder " << endl;
 		cout << "Jaime Mendoza de la ciudad de Potosi, el nivel " << endl;
-		cout << "primario lo realicé en la escuela Don Bosco de la" << endl;
-		cout << "ciudad de Potosi, el nivel secundario lo realicé en " << endl;
+		cout << "primario lo realicÃ© en la escuela Don Bosco de la" << endl;
+		cout << "ciudad de Potosi, el nivel secundario lo realicÃ© en " << endl;
 		cout << "el colegio Nacional Junin de la ciudad de Sucre, tengo" << endl;
-		cout << "un título en técnico medio en electricidad." << endl;
+		cout << "un tÃ­tulo en tÃ©cnico medio en electricidad." << endl;
 		cout << "Estudio la carrera de Ingenieria de Sistemas, mis" << endl;
 		cout << "pasatiempos son jugar videojuegos ,ayudar  a mi" << endl;
 		cout << "familia en casa, mi color favorito es el color negro." << endl;
 		cout << "No realizo ningun deporte debido a un  accidente" << endl;
-		cout << "que tuve hace unos años atrás, donde fue afectado mi "<< endl;
+		cout << "que tuve hace unos aÃ±os atrÃ¡s, donde fue afectado mi "<< endl;
 		cout << "pie izquierdo, mi juego favorito es Minecraft, uno de" << endl;
-		cout << "mis sueños es poder programar mods y videjuegos\n" << endl;
-		cout << "Desea volver a ver la biografía oprima la (v/V) Para salir oprima la (s/S) para salir" << endl;
-		cin >> v;
+		cout << "mis sueÃ±os es poder programar mods y videjuegos\n" << endl;
+		cout << "Desea volver a ver la biografÃ­a oprima la (s) Para salir oprima la (n) para salir" << endl;
+		cin >> s;
 
 	}
 	return 0;
